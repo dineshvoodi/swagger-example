e1b4bba52140547d955dbd5b2815ae6eec51c983
Dinesh Voodi <dineshvoodi@yahoo.com>
2019-08-02 18:11:39 +0530

    first commit

---
+++ src/main/java/com/example/swaggerdemo/SwaggerEampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerEampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerEampleApplication.class, args);
+	}
+
+}
